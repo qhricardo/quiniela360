3cdf72f5325217bb15c81c37a8dec6a671b6a855
qhricardo <125143902+qhricardo@users.noreply.github.com>
2025-06-23 09:56:53 -0600

    Update partidos-data.js

--- js/partidos-data.js
+++ js/partidos-data.js
@@ -1,12 +1,12 @@
 window.partidosProgramados = [
-  "P1 Inter Milan vs Urawa Red Diamonds ",
-  "P2 Fluminense vs Ulsan Hyundai",
-  "P3 River Plate vs Monterrey ",
-  "P4 Curazao  vs Canadá ",
-  "P5 Honduras vs El Salvador ",
-  "P6 Arabia Saudí vs Trinidad y Tobago ",
-  "P7 México vs Costa Rica ",
-  "P8 Real Madrid vs Pachuca  ",
-  "P9 Red Bull Salzburg vs Al Hilal ",
-  "P10 República Dominicana vs Surinam  "
+  "P1 Panamá  vs Jamaica  ",
+  "P2 Guadalupe  vs Guatemala ",
+  "P3 Honduras  vs Curazao  ",
+  "P4 Canadá   vs El Salvador ",
+  "P5 Benfica  vs FC Bayern ",
+  "P6 Auckland City vs Boca Juniors ",
+  "P7 Los Angeles FC vs CR Flamengo ",
+  "P8 Mamelodi Sundowns vs Fluminense  ",
+  "P9 Inter Milan vs River Plate ",
+  "P10 Urawa Red Diamonds vs Monterrey   "
 ];
